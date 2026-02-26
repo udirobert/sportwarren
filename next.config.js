df444fff87129d48b8b64014a5e25bb452177bf8
Quincybob <74205675+Quincybob@users.noreply.github.com>
2026-02-26 03:00:51 +0300

    fix: remove static export from next.config.js to support API routes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
